1c16f5e978d00eb30606e05047771a3f2e29613e
yangwen <1052045476@qq.com>
2018-04-10 14:35:15 +0800

    first

---
+++ springactivemqdemo/src/main/java/com/asgc/springactivemq/demo/mq/consumer/queue/QueueReceiver1.java
@@ -0,0 +1,21 @@
+package com.asgc.springactivemq.demo.mq.consumer.queue;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueReceiver1 implements MessageListener {
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("QueueReceiver1接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ springactivemqdemo/src/main/java/com/asgc/springactivemq/demo/mq/consumer/topic/TopicReceiver1.java
@@ -0,0 +1,22 @@
+package com.asgc.springactivemq.demo.mq.consumer.topic;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicReceiver1 implements MessageListener{
+
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("TopicReceiver1接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
